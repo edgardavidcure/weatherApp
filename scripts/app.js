bc592f673d832d7da39b0461b75b52b1bb515dfd
Edgar Cure <edgardavid1503@gmail.com>
2023-03-03 18:15:41 -0800

    Updated design

--- scripts/app.js
+++ scripts/app.js
@@ -40,7 +40,7 @@ if (option) option.addEventListener("change", (event) =>  {
 
 function drawWeather( d ) {
     var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-      var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+    var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
     const description = d.weather[0].description.split(" ")
     for (let i = 0; i < description.length; i++){
       description[i] = description[i][0].toUpperCase() + description[i].substr(1);
